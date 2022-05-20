9adf97d4e9c93f9fafc5f06c06c1d6b1e0246724
michael-lehmann <53788649+michael-lehmann@users.noreply.github.com>
2022-05-20 15:03:03 +0200

    Update get_scripts.sh

--- get_scripts.sh
+++ get_scripts.sh
@@ -1,2 +1,2 @@
-wget -O fetch_scrits.sh https://raw.githubusercontent.com/michael-lehmann/aws_lightsail_open_firewall/master/get_scripts.sh
-wget -O update_ip.sh https://raw.githubusercontent.com/michael-lehmann/aws_lightsail_open_firewall/master/update_ip.sh
+wget -O fetch_scrits.sh https://raw.githubusercontent.com/michael-lehmann/aws_utils/master/get_scripts.sh
+wget -O update_ip.sh https://raw.githubusercontent.com/michael-lehmann/aws_utils/master/update_ip.sh
