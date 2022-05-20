6ad5710035f77f168b51e9d3976f377ad9ade0ea
michael-lehmann <53788649+michael-lehmann@users.noreply.github.com>
2022-05-20 15:17:33 +0200

    Update get_scripts.sh

--- get_scripts.sh
+++ get_scripts.sh
@@ -1,2 +1,2 @@
-curl --output get_scripts.sh https://raw.githubusercontent.com/michael-lehmann/aws_utils/main/get_scripts.sh
-curl --output update_ip.sh https://raw.githubusercontent.com/michael-lehmann/aws_utils/main/update_ip.sh
+curl https://raw.githubusercontent.com/michael-lehmann/aws_utils/main/get_scripts.sh --output get_scripts.sh 
+curl https://raw.githubusercontent.com/michael-lehmann/aws_utils/main/update_ip.sh --output update_ip.sh 
